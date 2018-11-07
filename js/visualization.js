9ab9fcb25b29bc63949e18ab1a04eac359f373b8
Rikonman <rickiekidwell@yahoo.com>
2018-11-07 16:12:50 -0500

    Added race demographic pie charts and a few useful functions.

--- js/visualization.js
+++ js/visualization.js
@@ -7,10 +7,12 @@ var geoData;
 var geoGenerator;
 
 var countyDemoData;
-var pieRadius = 30;
-var colors = [ "Red", "Orange", "Yellow", "Green", "Blue", "Cyan", "Purple"];
+var pieRadius = 25;
+var colors = [ "Red", "Chartreuse", "Blue", "Brown", "Gold", "Cyan", "Blue"];
 var svg;
-
+var width;
+var height;
+var legendBlockSize = 20;
 //this function is complete
 function initializeVis(dataDemos, dataVoting, dataGeo) {
   demoData = dataDemos;
@@ -18,8 +20,8 @@ function initializeVis(dataDemos, dataVoting, dataGeo) {
   geoData = dataGeo;
   
   svg = d3.select('#svgMap');
-  let width = +svg.attr('width');
-    let height = +svg.attr('height');
+  width = +svg.attr('width');
+  height = +svg.attr('height');
 
     // Geo scaling of data
     let projection = d3.geoEquirectangular()
@@ -33,6 +35,15 @@ function getDemoRow(subject, title) {
 	for (var counter = 0; counter < demoData.length; counter++) {
 		if (demoData[counter]["Subject"] == subject && demoData[counter]["Title"] == title) {
 			return demoData[counter];
+		}
+	}
+}
+
+//this function is complete
+function getGeo(distNum) {
+	for (var counter = 0; counter < geoData.features.length; counter++) {
+		if (parseInt(geoData.features[counter].properties.CD115FP) == parseInt(distNum)) {
+			return geoData.features[counter];
 		}
 	}
 }
@@ -58,19 +69,84 @@ function generateDemographicPies() {
 		countyDemoData.push(countyRow);
 		var pieMaker = d3.pie();
 		var countyPie = pieMaker(countyRow);
-		var arcs = d3.arc().outerRadius(pieRadius);
+		var arcs = d3.arc()
+		.innerRadius(0)
+		.outerRadius(pieRadius);
 		var countyPieGroup = pieGroup.append("g");
 		countyPieGroup.selectAll("path")
 		.data(countyPie)
 		.enter()
 		.append("path")
-		.attr("d", arcs)
-		.attr("transform", function () {
-			return "translate (" + (-70 + counter * 80) + ", 40)";
+		.attr("transform", function (d, i) {
+			var center = geoGenerator.centroid(getGeo(counter));
+			return "translate (" + center + ")";
 		})
 		.attr("fill", function(d, i) {
 			return colors[i];
-		});
+		})
+		.attr("d", arcs);
 	}
+	makeLegend(svg);
+}
 
+function makeLegend(inSVG) {
+	//makes base area for legend
+	var legend = inSVG.append('g')
+		.attr("transform", "translate(" + (width - 200) + ", " + (height - 125) + ")");
+	
+	//White
+	legend.append("text")
+	.attr("transform", "translate(0, 15)")
+	.text("White")
+	.attr("fill", colors[0]);
+	legend.append("rect")
+	.attr("transform", "translate(175,0)")
+	.attr("width", legendBlockSize)
+	.attr("height", legendBlockSize)
+	.attr("fill", colors[0]);
+		
+	//Black or African American
+	legend.append("text")
+	.attr("transform", "translate(0, 40)")
+	.text("Black or African American")
+	.attr("fill", colors[1]);
+	legend.append("rect")
+	.attr("transform", "translate(175,25)")
+	.attr("width", legendBlockSize)
+	.attr("height", legendBlockSize)
+	.attr("fill", colors[1]);
+		
+	//Asian
+	legend.append("text")
+	.attr("transform", "translate(0, 65)")
+	.text("Asian")
+	.attr("fill", colors[2]);
+	legend.append("rect")
+	.attr("transform", "translate(175,50)")
+	.attr("width", legendBlockSize)
+	.attr("height", legendBlockSize)
+	.attr("fill", colors[2]);
+		
+	//Other Ethnicity
+	legend.append("text")
+	.attr("transform", "translate(0, 90)")
+	.text("Other Ethnicity")
+	.attr("fill", colors[3]);
+	legend.append("rect")
+	.attr("transform", "translate(175,75)")
+	.attr("width", legendBlockSize)
+	.attr("height", legendBlockSize)
+	.attr("fill", colors[3]);
+		
+	//Multiracial
+	legend.append("text")
+	.attr("transform", "translate(0, 115)")
+	.text("Multiracial")
+	.attr("fill", colors[4]);
+	legend.append("rect")
+	.attr("transform", "translate(175,100)")
+	.attr("width", legendBlockSize)
+	.attr("height", legendBlockSize)
+	.attr("fill", colors[4]);
+		
 }
